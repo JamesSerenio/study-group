38ffa0d80f201d2851a93535a286611f6c2cabee
JamesSerenio <sereniojames363@gmail.com>
2025-05-15 00:45:33 +0800

    Updates

--- assets/ios.transition-legacy-Tb554e0y.js
+++ assets/ios.transition-legacy-Tb554e0y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnoQXa2F.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CBbH8gl4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
